5d77fc757fbeafcedb896464dd3f47ce0b959672
Scott Taylor <scott@railsnewbie.com>
2012-02-06 12:19:14 -0500

    Add support for custom scp port.  Closes #35

--- lib/beta_builder/deployment_strategies/web.rb
+++ lib/beta_builder/deployment_strategies/web.rb
@@ -16,7 +16,7 @@ module BetaBuilder
           end
         end
       end
-      
+
       def prepare
         plist = CFPropertyList::List.new(:file => "pkg/Payload/#{@configuration.app_name}/Info.plist")
         plist_data = CFPropertyList.native_types(plist.value)
@@ -82,9 +82,20 @@ module BetaBuilder
           }
         end
       end
-      
+
       def deploy
-        system("scp pkg/dist/* #{@configuration.remote_host}:#{@configuration.remote_installation_path}")
+        cmd = []
+
+        cmd.push "scp"
+
+        if @configuration.remote_port
+          cmd.push "-P #{@configuration.remote_port}"
+        end
+
+        cmd.push "pkg/dist/*"
+        cmd.push "#{@configuration.remote_host}:#{@configuration.remote_installation_path}"
+
+        system(cmd.join(" "))
       end
     end
   end
